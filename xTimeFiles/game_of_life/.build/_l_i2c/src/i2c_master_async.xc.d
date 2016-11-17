32703d67248971a94ea013af183205a4a102cf80
Michael Todd <toddym42@hotmail.com>
2016-11-17 13:32:35 +0000

    removed build files. updated gitignore

--- xTimeFiles/game_of_life/.build/_l_i2c/src/i2c_master_async.xc.d
+++
@@ -1,5 +0,0 @@
-.build/_l_i2c/src/i2c_master_async.xc.d .build/_l_i2c/src/i2c_master_async.xc.o .build/_l_i2c/src/i2c_master_async.xc.pca.xml: \
-  /Users/toddym42/Dev/Concurrent\ Computing/TB1_Project/farmbit/xTimeFiles/lib_i2c/src/i2c_master_async.xc \
-  /Users/toddym42/Dev/Concurrent\ Computing/TB1_Project/farmbit/xTimeFiles/lib_i2c/api/i2c.h \
-  /Users/toddym42/Dev/Concurrent\ Computing/TB1_Project/farmbit/xTimeFiles/lib_xassert/api/xassert.h
-
