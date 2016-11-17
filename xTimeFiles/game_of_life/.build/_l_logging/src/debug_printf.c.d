781974824999afc2c794ac013d69a0cdff895001
Michael Todd <toddym42@hotmail.com>
2016-11-17 18:21:01 +0000

    removed build files

--- xTimeFiles/game_of_life/.build/_l_logging/src/debug_printf.c.d
+++
@@ -1,3 +0,0 @@
-.build/_l_logging/src/debug_printf.c.d .build/_l_logging/src/debug_printf.c.o .build/_l_logging/src/debug_printf.c.pca.xml: \
-  /Users/toddym42/Dev/Concurrent\ Computing/TB1_Project/cellular-automaton-farm/xTimeFiles/lib_logging/src/debug_printf.c \
-  /Users/toddym42/Dev/Concurrent\ Computing/TB1_Project/cellular-automaton-farm/xTimeFiles/lib_logging/api/debug_print.h
--- xTimeFiles/game_of_life/.build/src/main.xc.d
+++
@@ -1,4 +0,0 @@
-.build/src/main.xc.d .build/src/main.xc.o .build/src/main.xc.pca.xml: ./src/main.xc \
-  /Users/toddym42/Dev/Concurrent\ Computing/TB1_Project/cellular-automaton-farm/xTimeFiles/game_of_life/src/pgmIO.h \
-  /Users/toddym42/Dev/Concurrent\ Computing/TB1_Project/cellular-automaton-farm/xTimeFiles/lib_i2c/api/i2c.h
-
