f96b7c800393ba742eb2865ee2b1a5df3ae34d11
Michael Todd <toddym42@hotmail.com>
2016-11-17 18:25:35 +0000

    removed build files

--- xTimeFiles/game_of_life/.build/_l_i2c/src/i2c_master_async.xc.d
+++
@@ -1,5 +0,0 @@
-.build/_l_i2c/src/i2c_master_async.xc.d .build/_l_i2c/src/i2c_master_async.xc.o .build/_l_i2c/src/i2c_master_async.xc.pca.xml: \
-  /Users/toddym42/Dev/Concurrent\ Computing/cellular-automaton-farm/xTimeFiles/lib_i2c/src/i2c_master_async.xc \
-  /Users/toddym42/Dev/Concurrent\ Computing/cellular-automaton-farm/xTimeFiles/lib_i2c/api/i2c.h \
-  /Users/toddym42/Dev/Concurrent\ Computing/cellular-automaton-farm/xTimeFiles/lib_xassert/api/xassert.h
-
